0e7e36d4ea92bb39eddec03f236ee44aca9346f2
Hariom Jaiswal <omjaiswal942@gmail.com>
2026-06-26 10:25:23 +0530

    Fix CORS for Vercel

--- interview-ai/Backend/src/app.js
+++ interview-ai/Backend/src/app.js
@@ -1,27 +1,28 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
     origin: [
-        "http://localhost:5173",
-        "https://interview-ai-op2q.onrender.com"
+      "http://localhost:5173",
+      "https://ai-interview-prep-ten-tau.vercel.app",
     ],
-    credentials: true
-}))
-/* require all the routes here */
-const authRouter = require("./routes/auth.routes")
-const interviewRouter = require("./routes/interview.routes")
+    credentials: true,
+  })
+);
 
+/* require all the routes here */
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
 
 /* using all the routes here */
-app.use("/api/auth", authRouter)
-app.use("/api/interview", interviewRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
-
-
-module.exports = app
+module.exports = app;
